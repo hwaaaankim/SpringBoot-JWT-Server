ef91aa588e52d6705d6796659fdc98990c8d0347
thramania <thramania@hanmail.net>
2021-07-13 15:38:35 +0900

    project initialize

---
+++ src/main/java/com/dev/SecTwoJWT/SecTwoJwtApplication.java
@@ -0,0 +1,13 @@
+package com.dev.SecTwoJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecTwoJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecTwoJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/SecTwoJWT/SecTwoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.SecTwoJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecTwoJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
